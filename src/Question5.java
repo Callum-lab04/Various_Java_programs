ed1e24eb3a14c64a7c9592d6e0757dede64a76be
Callum McConnell <McConnell-C28@ulster.ac.uk>
2024-10-10 18:51:05 +0100

    initial commit.

--- src/Question5.java
+++ src/Question5.java
@@ -1,2 +1,39 @@
+import java.util.Scanner;
+
 public class Question5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a word: ");
+        String input = scanner.nextLine();
+
+        stringIncrement(input);
+        String reversedInput = reverseStringIncrement(input);
+
+        if (input.equalsIgnoreCase(reversedInput)) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is not a Palindrome.");
+        }
+
+        scanner.close();
+    }
+
+    static String stringIncrement(String input) {
+        input = input.toLowerCase();
+        for (int i = 0; i < input.length(); i++) {
+            char character = input.charAt(i);
+        }
+        return input;
+    }
+
+    static String reverseStringIncrement(String input) {
+        StringBuilder reversed = new StringBuilder();
+        input = input.toLowerCase();
+        for (int i = input.length() - 1; i >= 0; i--) {
+            char character = input.charAt(i);
+            reversed.append(character);
+        }
+        return reversed.toString();
+    }
 }
